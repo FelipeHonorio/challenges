e29d9cb02f6a7ec65605796a829adc99aea8cc38
Felipe Honório <felipe.h.camargo@gmail.com>
2019-08-04 16:19:39 -0300

    added hackerrank challenge

---
+++ hackerrank/arrays/01-leftRotation.js
@@ -0,0 +1,18 @@
+/** Arrays: Left Rotation
+ * 
+ * A left rotation operation on an array shifts each of the array's elements 1
+ * unit to the left. For example, if 2 left rotations are performed on array
+ * [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2] 
+ */
+
+function rotLeft(array, numRot){
+ for (let i = 0; i < numRot; i++) {
+   for (let j = 1; j < array.length; j++) {
+    const numberRemoved = array.splice(j, 1)[0];
+    array.splice(j - 1, 0, numberRemoved);
+   }
+ }
+ return array;
+}
+
+rotLeft([1,2,3,4,5], 2) //[3,4,5,1,2]
